166c627ea6db9f6c213f451ad050030946adfc6e
Kees-M <scg1813@aut.ac.nz>
2025-10-31 15:20:59 +1300

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package assignment_2;
 
 public class Player {
+
     private String name;
     private int score;
 
+    // Constructor to set player name and initialize score
     public Player(String name) {
         this.name = name;
         this.score = 0;
     }
 
-    public String getName() { return name; }
-    public int getScore() { return score; }
-    public void incrementScore() { score++; }
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    // Increment score after correct answer
+    public void incrementScore() {
+        score++;
+    }
 }
