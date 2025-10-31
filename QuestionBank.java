233afd322898f55c961c7f6523a599ecebaa51e3
Kees-M <scg1813@aut.ac.nz>
2025-10-31 15:24:19 +1300

    Update QuestionBank.java

--- QuestionBank.java
+++ QuestionBank.java
@@ -1,31 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package assignment_2;
 
-/**
- *
- * @author OEM
- */
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class QuestionBank {
+
     private List<Question> questions;
 
+    // Default constructor for building the bank dynamically
+    public QuestionBank() {
+        this.questions = new ArrayList<>();
+    }
+
+    // Optional constructor to initialize with a list of questions
     public QuestionBank(List<Question> questions) {
-        this.questions = questions;
+        this.questions = new ArrayList<>(questions);
+        Collections.shuffle(this.questions); // Shuffle for randomness
+    }
+
+    // Add new questions and reshuffle
+    public void addQuestions(List<Question> newQuestions) {
+        this.questions.addAll(newQuestions);
         Collections.shuffle(this.questions);
     }
 
+    // Retrieve question by index
     public Question getQuestion(int index) {
         return questions.get(index);
     }
 
+    // Get total number of questions
     public int size() {
         return questions.size();
     }
 }
-
-
